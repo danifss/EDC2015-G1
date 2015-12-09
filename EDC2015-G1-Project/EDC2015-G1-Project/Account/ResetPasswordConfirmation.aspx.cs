0a3f845c63eaa5fc9283e70111a391743cbca367
Daniel <daniel.silva@ua.pt>
2015-12-09 23:29:06 +0000

    Initial release

---
+++ EDC2015-G1-Project/EDC2015-G1-Project/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EDC2015_G1_Project.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EDC2015-G1-Project/EDC2015-G1-Project/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EDC2015_G1_Project.Startup))]
+namespace EDC2015_G1_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
